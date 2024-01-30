5ec722182cbc7676310ec66626447b2b1048e50e
jaysaavedra18 <jasonsaavedra@yahoo.com>
2024-01-30 14:26:08 -0500

    Added firebase dependencies to package.json, run npm install to get uptodate on packages. Added Login and Register views to the router but not yet functional

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,29 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCJDCuWGVbNLA75kWz2Wt_E32SxahOjgEw",
+  authDomain: "livin-cs407.firebaseapp.com",
+  projectId: "livin-cs407",
+  storageBucket: "livin-cs407.appspot.com",
+  messagingSenderId: "42343253132",
+  appId: "1:42343253132:web:a04069c21c9b8c9cbd67a9",
+  measurementId: "G-DPBD6Z65GJ"
+};
+
+// Initialize Firebase and Authentication
+const firebaseapp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseapp)
+
+
 const app = createApp(App)
 
 app.use(createPinia())
---
+++ src/views/Register.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="register">
+        Register
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
