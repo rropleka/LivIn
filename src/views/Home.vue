ca72d729cabd66898b18b5afb8169b05a58acc7e
afshirazi <ashirazi@purdue.edu>
2024-01-31 16:13:25 -0500

    Added Tailwind/Flowbite dependencies and rudimentary navbar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router';
+import Navbar from '../components/navbar-comp.vue';
 </script>
 
 <template>
   <main>
+    <Navbar />
     <router-link to="/login">bbbbb</router-link>
   </main>
 </template>
