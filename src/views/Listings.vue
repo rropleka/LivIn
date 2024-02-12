b6691c07b61db770b3cac63487c56e93455a9e59
Tommy Nguyen <tommyhnguyen121@gmail.com>
2024-02-12 04:11:58 -0500

    Create basic listings table with basic data

--- src/views/Listings.vue
+++ src/views/Listings.vue
@@ -1,13 +1,33 @@
-<script setup>
+<script>
     //import Listing.js from "/objects/Listing.js";
     //Listing1 = new Listing('Property1');
     import router from '../router/index'
+    export default {
+  setup(){
+    const listings = [
+            { propertyName: 'Lafayette', address: '100 Lafayette Way', price: '$100' },
+            { propertyName: 'Wiley Hall', address: '200 Wiley St', price: '$200' },
+            { propertyName: 'Cedarwood', address: '300 Grant St', price: '$300' },
+        ]
+    return{listings}
+    },
+}
 </script>
 
 <template>
     <div class="listings">
-        <div class="listing">
-            <p style="color: black;">Hi</p>
-        </div>
+        <table>
+            <tbody>
+            <tr v-for="listing in listings" :key="listing.propertyName">
+                <td>
+                    <div style="width: 400px; height: 150px; border: 1px solid black; color: black;">
+                        Property Name: {{ listing.propertyName }} <br>
+                        Address: {{ listing.address }} <br>
+                        Price: {{ listing.price }} <br>
+                    </div>
+                </td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 </template>
