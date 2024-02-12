729587cd6e988321e08cec5196f1fbf1600a40a5
amgazda <67490541+amgazda@users.noreply.github.com>
2024-02-12 13:27:28 -0500

    Minor changes

--- src/views/PropRev.vue
+++ src/views/PropRev.vue
@@ -1,11 +1,16 @@
 <template>
+  <div class="score">
+    <h1>Cumulative Scores</h1>
+    <h2>{{ loadPack.totalReviews }}</h2>
+  </div>
   <form @submit.prevent="prev">
       <div class="leaveRev">
           <h1>Leave a Review</h1>
           <h2>Enter a star rating</h2>
-          <input type="number" placeholder="Stars" v-model="form.stars"><br/>
+          <input type="number" placeholder="Stars" v-model="form.stars" min="1" max="5"><br/>
           <h2>Enter a review</h2>
-          <input type="text" placeholder="Review" v-model="form.text">
+          <!--<input type="text" placeholder="Review" v-model="form.text">-->
+          <textarea v-model="form.text"></textarea>
           <br>
           <button v-on:click="sub" type="submit">Submit review</button>
       </div>
@@ -79,7 +84,7 @@
   .leaveRev h2 {
     color:red;
   }
-  input[type='text'],input[type='number']{
+  input[type='text'],input[type='number'],textarea{
     color: black;
     border-width: 3px;
     border-style: dashed;
@@ -93,6 +98,21 @@
     padding: 5px;
     color: white;
     border-radius: 10px;
+  }
+  button[type='submit']:hover {
+    background-color: lightcoral;
+  }
+  .score {
+    align-items: center;
+    border-width: 5px;
+    border-style: dashed;
+    border-color: orange;
+    border-radius: 4px;
+    width: fit-content;
+    height: fit-content;
+    margin: 10px;
+    padding: 10px;
+    color: black;
   }
 }
 </style>
