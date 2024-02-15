842ca6706531e049b19b81bff83ff831db39ffeb
MythicalChromosome <70397434+MohiniPanini@users.noreply.github.com>
2024-02-15 02:45:01 -0500

    resize map

--- src/components/GMapItem.vue
+++ src/components/GMapItem.vue
@@ -6,21 +6,27 @@
     // eslint-disable-next-line vue/no-reserved-component-names
     components: { GoogleMap, Marker },
     setup() {
-      const center = { lat: 40.4259, lng: -86.9081 };
+      const center = { lat: 40.420781, lng: -86.918061 };
       return { center };
     },
+    computed: {
+      style() {
+        const mapHeight = window.screen.height - 260;
+        return {
+          width: '100%',
+          height: mapHeight+'px',
+        }
+      }
+    }
   });
 </script>
 
 <template>
-  <GoogleMap api-key="AIzaSyAuAji5VLjhvBMxeLE5SMjVJA4soq1JZK8" style="width: 100%; height: 565px" :center="center" :zoom="15">
+  <GoogleMap api-key="AIzaSyAuAji5VLjhvBMxeLE5SMjVJA4soq1JZK8" :style=style :center="center" :zoom="15">
     
   </GoogleMap>
 </template>
 
 <style scoped>
-  GoogleMap {
-    width: 100%;
-    height: 500px;
-  }
+
 </style>
