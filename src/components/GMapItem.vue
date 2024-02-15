2e4c66355f62e142f8a4d7990b7f3248f9c87ab5
MythicalChromosome <70397434+MohiniPanini@users.noreply.github.com>
2024-02-15 03:44:39 -0500

    added left click and right click events

--- src/components/GMapItem.vue
+++ src/components/GMapItem.vue
@@ -1,10 +1,17 @@
 <script lang="ts">
   import {GoogleMap, Marker} from "vue3-google-map";
   import {defineComponent} from "vue";
+  import {MapMouseEvent} from "google.maps";
 
   export default defineComponent({
     // eslint-disable-next-line vue/no-reserved-component-names
     components: { GoogleMap, Marker },
+    data() {
+      return {
+        point1: null,
+        point2: null,
+      }
+    },
     setup() {
       const center = { lat: 40.420781, lng: -86.918061 };
       return { center };
@@ -17,12 +24,30 @@
           height: mapHeight+'px',
         }
       }
+    },
+    methods: {
+      onMapClick(event: MapMouseEvent) {
+        this.point1 = event.latLng.lat();
+        console.log(this.point1);
+        console.log(this.point2);
+      },
+      onMapRightClick(event) {
+        this.point2 = event.latLng.lat();
+        console.log(this.point1);
+        console.log(this.point2);
+      }
     }
   });
 </script>
 
 <template>
-  <GoogleMap api-key="AIzaSyAuAji5VLjhvBMxeLE5SMjVJA4soq1JZK8" :style=style :center="center" :zoom="15">
+  <GoogleMap 
+    api-key="AIzaSyAuAji5VLjhvBMxeLE5SMjVJA4soq1JZK8" 
+    :style=style 
+    :center="center" 
+    @click="onMapClick"
+    @contextmenu="onMapRightClick"
+    :zoom="15">
     
   </GoogleMap>
 </template>
