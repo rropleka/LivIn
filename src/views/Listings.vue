5112bfdd1fa6ee1b7517d4b860395fce472161dc
Tommy Nguyen <tommyhnguyen121@gmail.com>
2024-02-17 03:16:59 -0500

    Add basic sorting functionality

--- src/views/Listings.vue
+++ src/views/Listings.vue
@@ -2,7 +2,7 @@
     //import Listing.js from "/objects/Listing.js";
     //Listing1 = new Listing('Property1');
     import router from '../router/index'
-    export default {
+    /*export default {
   setup(){
     const listings = [
             { propertyName: 'Lafayette', address: '100 Lafayette Way', price: '$100' },
@@ -10,11 +10,67 @@
             { propertyName: 'Cedarwood', address: '300 Grant St', price: '$300' },
         ]
     return{listings}
-    },
-}
+    },*/
+    export default {
+    
+        data: function(){
+            return{
+            listings:[
+            { propertyName: 'Lafayette', address: '100 Lafayette Way', price: '$200' },
+            { propertyName: 'Wiley Hall', address: '200 Wiley St', price: '$100' },
+            { propertyName: 'Cedarwood', address: '300 Grant St', price: '$300' },
+        ],
+            currentSort:'name',
+            currentSortDir:'asc',
+            pageSize:3,
+            currentPage:1
+            };
+        },
+        /*created:function() {
+            this.listings = [
+            { propertyName: 'Lafayette', address: '100 Lafayette Way', price: '$100' },
+            { propertyName: 'Wiley Hall', address: '200 Wiley St', price: '$200' },
+            { propertyName: 'Cedarwood', address: '300 Grant St', price: '$300' },
+        ]
+        return{listings}
+        },*/
+        methods:{
+            sort:function(s) {
+            //if s == current sort, reverse
+            if(s === this.currentSort) {
+                this.currentSortDir = this.currentSortDir==='asc'?'desc':'asc';
+            }
+            this.currentSort = s;
+            }
+        },
+        computed:{
+            listings:function() {
+            return this.listings.sort((a,b) => {
+                let modifier = 1;
+                if(this.currentSortDir === 'desc') modifier = -1;
+                if(a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
+                if(a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
+                return 0;
+            });
+            }
+        }
+
+    }
+//}
 </script>
 
+<style>
+    .listingtext {
+        color: black;
+    }
+</style>
+
 <template>
+    <div class="listingtext">
+        <button @click="sort('propertyName')">Property Name</button>
+        <button @click="sort('address')">Address</button>
+        <button @click="sort('price')">Price</button>
+    </div>
     <div class="listings">
         <table>
             <tbody>
