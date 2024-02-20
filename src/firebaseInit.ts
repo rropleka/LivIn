0a9ee77f757b0e9660e18dc17715a90ebf569955
jaysaavedra18 <jasonsaavedra@yahoo.com>
2024-02-20 00:41:16 -0500

    fixed glbl user login on registration, completed email verification on register/modification, and purdue verification icon is working

--- src/firebaseInit.ts
+++ src/firebaseInit.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import firebase from 'firebase'
+// import firebase from 'firebase'
 import { initializeApp } from "firebase/app"
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
