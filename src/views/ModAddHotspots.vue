22ad6d85bb2c3b131872aed45fc35577a2e986c0
MythicalChromosome <70397434+MohiniPanini@users.noreply.github.com>
2024-02-21 03:57:32 -0500

    interface to add hotspots

--- src/views/ModAddHotspots.vue
+++ src/views/ModAddHotspots.vue
@@ -1,16 +1,61 @@
 <script lang="ts">
+import { getFirestore, collection, doc, getDocs, setDoc, addDoc, query, where } from 'firebase/firestore/lite'
+import { firebaseapp } from '../firebaseInit'
+import {defineComponent} from "vue";
+
+
+export default defineComponent ({
+    setup() {
+        return {
+            db: getFirestore(firebaseapp),
+            campus: "",
+            hotspot: ""
+        }
+    },
+    methods: {
+        async addHotspot() {
+            if (this.campus == "" || this.hotspot == "") {
+                console.log("Blank campus or hotspot");
+                return;
+            }
+            let coords = {lat: 999, lng: 999};
+
+            const splitString = this.hotspot.split(" ");
+            coords.lat = parseFloat(splitString[0]);
+            coords.lng = parseFloat(splitString[1]);
+            if (isNaN(coords.lat) || isNaN(coords.lng)) {
+                console.log("Improperly formatted coordinates");
+                return;
+            }
+            console.log("Can add hotspot");
+
+            const hotspotsRef = collection(this.db, "hotspots");
+            await addDoc(hotspotsRef, {
+                campus: this.campus,
+                coords: coords
+            })
+        }
+    }
+})
 
 </script>
 
 <template>
     <main>
         <h1>Moderator: Add Hotspots for Campus</h1>
-        <form @submit.prevent="register">
-            <input type="text" placeholder="Email" v-model="email" class="black-text"/>
-            <input type="password" placeholder="Password" v-model="password" class="black-text"/>
-            <input type="text" placeholder="Username" v-model="username" class="black-text"/>
-            <button type="submit">Register</button>
-            <p style="color: black;">Have an account? <router-link to="/login">Login Here</router-link></p>
+        <form @submit.prevent="addHotspot">
+            <input type="text" placeholder="Campus" v-model="campus" class="black-text"/>
+            <input type="text" placeholder="Hotspot" v-model="hotspot" class="black-text"/>
+            <button type="submit">Add Hotspot</button>
         </form>
     </main>
 </template>
+
+<style scoped>
+    form, h1 {
+        color: black;
+        display: flex;
+        flex-direction: column;
+        width: 33%;
+    }
+</style>
