148e9049eabd3fb84cb1d2a7a8779729258b2b06
MythicalChromosome <70397434+MohiniPanini@users.noreply.github.com>
2024-03-24 03:30:41 -0400

    removing lists works

--- src/views/ListsPage.vue
+++ src/views/ListsPage.vue
@@ -14,8 +14,15 @@
         </div>
         <div class="mainSection" v-else>
             <div class="listSection" v-for="(list, id) in lists" :key="list.listName">
-                <div class="listName">
+                <div class="listHeading">
                     {{list.listName}}
+
+                    <div class="headingButtons">
+                        <!-- trashcan -->
+                        <svg @click="removeList(id)" fill="#FFFFFF" width="20px" height="20px" viewBox="-6.7 0 122.88 122.88" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 109.484 122.88" xml:space="preserve">
+                            <g><path fill-rule="evenodd" clip-rule="evenodd" d="M2.347,9.633h38.297V3.76c0-2.068,1.689-3.76,3.76-3.76h21.144 c2.07,0,3.76,1.691,3.76,3.76v5.874h37.83c1.293,0,2.347,1.057,2.347,2.349v11.514H0V11.982C0,10.69,1.055,9.633,2.347,9.633 L2.347,9.633z M8.69,29.605h92.921c1.937,0,3.696,1.599,3.521,3.524l-7.864,86.229c-0.174,1.926-1.59,3.521-3.523,3.521h-77.3 c-1.934,0-3.352-1.592-3.524-3.521L5.166,33.129C4.994,31.197,6.751,29.605,8.69,29.605L8.69,29.605z M69.077,42.998h9.866v65.314 h-9.866V42.998L69.077,42.998z M30.072,42.998h9.867v65.314h-9.867V42.998L30.072,42.998z M49.572,42.998h9.869v65.314h-9.869 V42.998L49.572,42.998z"/></g>
+                        </svg>
+                    </div>
                 </div>
                 <div class="listEntry" v-for="entry in list.entries" :key="entry.entryName">
                     <span>{{ entry.entryName }}:</span>
@@ -26,9 +33,9 @@
     </div>
 </template>
 
-<script type="ts">
+<script lang="ts">
     import { useStore } from "vuex";
-    import { getFirestore, collection, doc, getDocs, setDoc, addDoc, query, where } from 'firebase/firestore/lite'
+    import { getFirestore, collection, doc, getDocs, setDoc, addDoc, query, where, deleteDoc } from 'firebase/firestore/lite'
     import { firebaseapp } from '../firebaseInit'
     export default {
         async setup() {
@@ -41,7 +48,7 @@
             }
 
             const listsSnapshot = await getDocs(query(collection(db, "lists"), where("user", "==", currentUser)));
-            let lists = {};
+            let lists: {[key: string]: Object} = {};
             listsSnapshot.forEach((doc) => {
                 try {
                     lists[doc.id] = doc.data();
@@ -116,7 +123,7 @@
                 this.render = false;
 
                 const listsSnapshot = await getDocs(query(collection(this.db, "lists"), where("user", "==", this.currentUser)));
-                let lists = {};
+                let lists: {[key: string]: Object} = {};
                 listsSnapshot.forEach((doc) => {
                     try {
                         lists[doc.id] = doc.data();
@@ -130,9 +137,22 @@
                 console.log(this.lists);
 
                 this.render = true;
+            },
+            async removeList(id: string) {
+                console.log(id);
+
+                const docToDelete = doc(this.db, "lists", id)
+                console.log(`${docToDelete}`);
+                await deleteDoc(docToDelete)
+                .catch((error: any) => {
+                    console.log(error);
+                })
+
+                this.reload();
             }
         }
     }
+
 </script>
 
 <style scoped>
@@ -200,5 +220,16 @@
         display: flex;
         flex-direction: row;
         flex-wrap: wrap;
+    }
+
+    .listHeading {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    svg {
+        cursor: pointer;
     }
 </style>
