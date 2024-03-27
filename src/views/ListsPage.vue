098a61c9709c4268cd20965c9bcadcbfe475ec5f
MythicalChromosome <70397434+MohiniPanini@users.noreply.github.com>
2024-03-26 23:59:16 -0400

    display shared lists

--- src/views/ListsPage.vue
+++ src/views/ListsPage.vue
@@ -8,7 +8,6 @@
             </div>
         </div>
 
-
         <div class="listSection" v-if="Object.keys(lists).length == 0">
             No lists
         </div>
@@ -88,6 +87,38 @@
                 </div>
             </div>
         </div>
+        <div class="sharedlists" v-for="user in Object.keys(sharedLists)" :key="user">
+            <div class="pageHeader">
+                <h1> {{user}}'s Lists </h1>
+            </div>
+            <div class="listSection" v-for="(list, id) in sharedLists[user]" :key="list.listName">
+                <div class="listHeading">
+                    <div class="headingButtons" v-if="listToRename != id">
+                        <span>{{list.listName}}</span>
+                    </div>
+
+                    <div class="headingButtons">
+                        <!-- trashcan -->
+                        <!-- implement removing shared lists from your view only?-->
+                        <!-- 
+                        <svg @click="" fill="#FFFFFF" width="20px" height="20px" viewBox="-6.7 0 122.88 122.88" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 109.484 122.88" xml:space="preserve">
+                            <g><path fill-rule="evenodd" clip-rule="evenodd" d="M2.347,9.633h38.297V3.76c0-2.068,1.689-3.76,3.76-3.76h21.144 c2.07,0,3.76,1.691,3.76,3.76v5.874h37.83c1.293,0,2.347,1.057,2.347,2.349v11.514H0V11.982C0,10.69,1.055,9.633,2.347,9.633 L2.347,9.633z M8.69,29.605h92.921c1.937,0,3.696,1.599,3.521,3.524l-7.864,86.229c-0.174,1.926-1.59,3.521-3.523,3.521h-77.3 c-1.934,0-3.352-1.592-3.524-3.521L5.166,33.129C4.994,31.197,6.751,29.605,8.69,29.605L8.69,29.605z M69.077,42.998h9.866v65.314 h-9.866V42.998L69.077,42.998z M30.072,42.998h9.867v65.314h-9.867V42.998L30.072,42.998z M49.572,42.998h9.869v65.314h-9.869 V42.998L49.572,42.998z"/></g>
+                        </svg>
+                        -->
+                    </div>
+                </div>
+
+                <div class="listEntry" v-for="entry in list.entries" :key="entry.entryName">
+                    <div class="listHeading">
+                        <span>{{ entry.entryName }}:</span>
+                        <span>{{ entry.entryLocation.lat }} {{ entry.entryLocation.lng }}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div v-else>
+        Loading...
     </div>
 </template>
 
@@ -299,6 +330,7 @@
         display: flex;
         justify-content: space-between;
         padding-inline: 10px;
+        align-items: flex-end;
     }
 
     .page {
@@ -369,8 +401,8 @@
         flex-direction: row;
     }
 
-    input {
-
+    .sharedlists {
+        margin-top: 32px;
     }
 
 </style>
