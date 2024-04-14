891b7842a0e5a9202230442c6b2a2d55889ffd83
afshirazi <ashirazi@purdue.edu>
2024-04-14 01:02:37 -0400

    added basic draggable component

---
+++ src/components/fav-users.vue
@@ -0,0 +1,43 @@
+<script>
+import draggable from "vuedraggable"
+import { ref } from "vue"
+
+export default {
+  components: {
+    draggable,
+  },
+  setup() {
+    const list = ref([
+      { id: 1, name: "John" },
+      { id: 2, name: "Doe" },
+      { id: 3, name: "Jane" },
+      { id: 4, name: "Doe" }
+    ])
+
+    return {
+      list,
+    }
+  },
+  data() {
+    return{
+      drag: false
+    }
+  }
+}
+
+</script>
+
+<template>
+	<draggable 
+		v-model="list" 
+		@start="drag=true" 
+		@end="drag=false" 
+		@change="console.log(list)"
+		item-key="id">
+		<template #item="{element}">
+			<div>
+				{{ element.name }}
+			</div>
+		</template>
+	</draggable>
+</template>
