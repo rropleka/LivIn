0553fc1a32ea0cc2db5ea87dd8d7d990bc2d78a8
amgazda <67490541+amgazda@users.noreply.github.com>
2024-04-16 13:10:54 -0400

    form and toggle button

--- src/views/Moneys.vue
+++ src/views/Moneys.vue
@@ -1,6 +1,27 @@
 <template>
     <h1>Calculate living expenses</h1>
-    
+    <button type="button" @click="toggle">Toggle</button>
+    <div class="manual" id="manual">
+        <form>
+            <h2>Rent</h2>
+            <input type="number" v-model="form.rent" placeholder="" min="0">
+            <h2>Food and Drink</h2>
+            <input type="number" v-model="form.food" placeholder="" min="0">
+            <h2>Transportation</h2>
+            <input type="number" v-model="form.transport" placeholder="" min="0">
+            <h2>Education</h2>
+            <input type="number" v-model="form.education" placeholder="" min="0">
+            <h2>Health</h2>
+            <input type="number" v-model="form.health" placeholder="" min="0">
+            <h2>Bills</h2>
+            <input type="number" v-model="form.bills" placeholder="" min="0">
+            <h2>Other</h2>
+            <input type="number" v-model="form.other" placeholder="" min="0">
+        </form>
+    </div>
+    <div class="auto" id="auto" hidden="hidden">
+        <h2>Hello World</h2>
+    </div>
 </template>
 
 <script>
@@ -18,6 +39,9 @@ export default {
     },
     data() {
         return {
+            form: {
+
+            },
             loadPack: {
                 
             }
@@ -33,12 +57,32 @@ export default {
             alert(errorMessage);
         }
 
+    },
+    methods: {
+        toggle() {
+            let manual = document.getElementById("manual");
+            let auto = document.getElementById("auto");
+            let hidden = manual.getAttribute("hidden");
+            if(hidden) {
+                auto.setAttribute("hidden", "hidden");
+                manual.removeAttribute("hidden");
+            } else {
+                manual.setAttribute("hidden", "hidden");
+                auto.removeAttribute("hidden");
+            }
+        }
     }
 };
 </script>
 
 <style>
 @media (min-width: 1024px) {
+h1{
+    color: black;
+}
+h2{
+    color: black;
+}
 
 }
 </style>
