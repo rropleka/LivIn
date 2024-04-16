590cf7332b6ff1ae53e596da6cf9eab821a14884
afshirazi <ashirazi@purdue.edu>
2024-04-16 12:59:09 -0400

    stylize and added buttons

--- src/components/fav-users.vue
+++ src/components/fav-users.vue
@@ -1,42 +1,61 @@
 <script>
-import draggable from "vuedraggable"
-import { ref } from "vue"
+	import draggable from "vuedraggable"
+	import { ref } from "vue"
 
-export default {
-  components: {
-    draggable,
-  },
-  setup() {
-    const list = ref([
-      { id: 1, name: "John" },
-      { id: 2, name: "Doe" },
-      { id: 3, name: "Jane" },
-      { id: 4, name: "Doe" }
-    ])
+	export default {
+	components: {
+		draggable,
+	},
+	setup() {
+		const list = ref([
+		{ id: 1, name: "John", age: 20, class: 3, gender: 'male', username: 'john_doe'},
+		{ id: 2, name: "Doe", age: 21, class: 2, gender: 'male', username: 'jdoe'},
+		{ id: 3, name: "Jane", age: 22, class: 1, gender: 'female', username: 'jane_doe'},
+		{ id: 4, name: "Doe", age: 23, class: 4, gender: 'female', username: 'doe2'}
+		])
 
-    return {
-      list,
-    }
-  },
-  data() {
-    return{
-      drag: true
-    }
-  }
+		return {
+			list,
+		}
+	},
+	data() {
+		return{
+			drag: true
+		}
+	}
 }
 
 </script>
 
 <template>
-	<draggable 
-		v-model="list" 
-		:disabled="!drag"
-		@change="console.log(list)"
-		item-key="id">
-		<template #item="{element}">
-			<div>
-				{{ element.name }}
-			</div>
-		</template>
-	</draggable>
+	<p class="font-default-font text-2xl font-medium text-center">Favorite users</p>
+	<div class="grid grid-cols-5 p-6">
+		<div></div>
+		<draggable 
+			v-model="list" 
+			:disabled="!drag"
+			item-key="id"
+			class="col-span-3">
+			<template #item="{element}">
+				<div class=" flex flex-row justify-between px-4 py-2 my-2 border-2 border-orange-200 shadow-md rounded-md">
+					<div class="flex flex-col">
+						<p class="text-lg font-medium text-gray-900">{{ element.name }}</p>
+						<p class="text-lg font-medium text-gray-900">Age: {{ element.age }} </p>
+						<p class="text-lg font-medium text-gray-900">Class: {{ element.class }}</p>
+						<p class="text-lg font-medium text-gray-900">Gender: {{ element.gender }}</p>
+					</div>
+					<div class="flex flex-col justify-center">
+						<button class="text-white bg-light-orange hover:bg-dark-orange font-medium rounded-lg text-sm px-8 py-2">
+							<router-link :to="{ name: 'user-page', params: { username: element.username } }">
+								View
+							</router-link>
+						</button>
+						<button class="text-white bg-light-orange hover:bg-dark-orange font-medium rounded-lg text-sm px-8 py-2">
+							Remove
+						</button>
+					</div>
+				</div>
+			</template>
+		</draggable>
+	</div>
 </template>
